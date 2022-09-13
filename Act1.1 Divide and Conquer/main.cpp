13feb0f76b75e76ea63ca448d011825856ebb134
Carlo <carlouxljlj@gmail.com>
2022-09-13 13:57:09 -0500

    s

--- Act1.1 Divide and Conquer/main.cpp
+++ Act1.1 Divide and Conquer/main.cpp
@@ -6,18 +6,18 @@
 
 using namespace std;
 
-void merge(vector<double> &lista, int start, int middle, int end){
+void merge(vector<double> &lista, int start, int middle, int end) {
     int leftIndex = start;
     int rightIndex = middle+1;
     int temporalIndex = 0;
     vector<double> auxiliarList;
     
-    while(leftIndex <= middle && rightIndex <= end){
-        if(lista[leftIndex] > lista[rightIndex]){
+    while (leftIndex <= middle && rightIndex <= end) {
+        if (lista[leftIndex] > lista[rightIndex]) {
             auxiliarList.push_back(lista[leftIndex]);
             leftIndex++;
             temporalIndex++;
-        }else{
+        } else {
             auxiliarList.push_back(lista[rightIndex]);
             rightIndex++;
             temporalIndex++;
@@ -40,7 +40,7 @@ void merge(vector<double> &lista, int start, int middle, int end){
 }
 
 void mergeSort(vector<double> &lista, int start, int end) {
-    if(start < end) {
+    if (start < end) {
         int middle = (start+end) / 2;
         mergeSort(lista, start, middle);
         mergeSort(lista, middle+1, end);
@@ -49,7 +49,7 @@ void mergeSort(vector<double> &lista, int start, int end) {
     return;
 }
 
-int main(){
+int main() {
     int n;
     cout << "Enter the number of values to order" << endl;
     cin >> n;
@@ -60,7 +60,7 @@ int main(){
         return 0;
     }
 
-    for(int i = 0 ; i < n ; i++) {
+    for (int i = 0 ; i < n ; i++) {
         double number;
         cout << "Enter number: " << i+1 << endl;
         cin >> number;
@@ -71,7 +71,7 @@ int main(){
     cout << "-----------------------------------------" << endl;
     cout << "Numbers before ordering" << endl;
     cout << "-----------------------------------------" << endl;
-    for (int i = 0 ; i < n ; i++){
+    for (int i = 0 ; i < n ; i++) {
         cout << numbers[i] << " ";
     }
     cout << endl << endl;
@@ -81,8 +81,9 @@ int main(){
     cout << "-----------------------------------------" << endl;
     cout << "Numbers after ordering (descending order)" << endl;
     cout << "-----------------------------------------" << endl;
-    for (int i = 0 ; i < n ; i++){
+    for (int i = 0 ; i < n ; i++) {
         cout << numbers[i] << " ";
     }
     cout << endl << endl;
+    return 0;
 }
